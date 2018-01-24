04401749797f22f92dc1bd406f4d8000dd52c3d2
Lucas Teixeira Rocha <lucasrochabr@outlook.com>
2018-01-24 12:53:16 -0300

    Everything is a component

--- app/App.js
+++ app/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import { Alert, AppRegistry, StyleSheet, View } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
-export default class HolisticatorAPP extends Component {
-  state = {
-    ready: false,
-    ledIsOn: false,
-    isSunny: 0,
-    doorIsOpen: false,
-    thereIsMovement: false,
-  };
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from 'native-base';
 
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-    });
-    this.setState({ ready: true });
-    setInterval(() => {
-      this._updateLedStatus();
-      this._updateReedStatus();
-      this._updateLuxSensorStatus();
-      this._updateMotionSensorStatus();
-    }, 5000);
-  }
+export default class ChickenRunAPP extends Component {
+  state = {
+    ready: 0,
+    ledIsOn: 0,
+    isSunny: 0,
+    doorIsOpen: 0,
+    thereIsMovement: 0,
+  };
 
   remoteCommand = (command, cb) => {
     return fetch('http://172.20.10.181:3000/api/robots/Robot%201/commands/' + command)
@@ -48,45 +48,76 @@ export default class HolisticatorAPP extends Component {
   _toogleLed = () => this.remoteCommand('led_toggle', this._updateLedStatus);
 
   render() {
+    if (!this.state.ready) {
+      return (
+        <Container>
+          <Text>Loading...</Text>
+        </Container>
+      );
+    }
     const icons = {
       led: (this.state.ledIsOn) ? "ios-bulb-outline" : "ios-bulb",
       sunny: (this.state.isSunny > 100) ? "ios-sunny-outline" : "ios-sunny",
       motion: (this.state.thereIsMovement) ? "ios-walk" : "ios-remove-circle",
       door: (this.state.doorIsOpen)
-        ? (<Icon name="ios-alert">
-            <Text>the door is open</Text>
-          </Icon>)
+        ? <Icon name="ios-alert"> <Text>the door is open</Text> </Icon>
         : null
     };
-    return (this.state.ready) ? (
+    return (
       <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <Icon name={icons.led} onPress={this._toogleLed}/>
-          <Icon name={icons.sunny}/>
-          <Icon name={icons.motion}/>
-          {icons.door}
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
+        <ChickenRunHeader/>
+        <ChickenRunContent
+          led={icons.led}
+          sunny={icons.sunny}
+          motion={icons.motion}
+          door={icons.door}
+          toogleLed={this._toogleLed}/>
       </Container>
-    ) : (
-      <Text>Loading...</Text>
+    );
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ ready: true });
+    setInterval(() => {
+      this._updateLedStatus();
+      this._updateReedStatus();
+      this._updateLuxSensorStatus();
+      this._updateMotionSensorStatus();
+    }, 5000);
+  }
+}
+
+export class ChickenRunHeader extends Component {
+  render() {
+    return (
+      <Header>
+        <Left>
+          <Button transparent>
+            <Icon name='menu' />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Header</Title>
+        </Body>
+        <Right />
+      </Header>
+    );
+  }
+}
+
+export class ChickenRunContent extends Component {
+  render() {
+    return (
+      <Content>
+        <Icon name={this.props.led} onPress={this.props.toogleLed}/>
+        <Icon name={this.props.sunny}/>
+        <Icon name={this.props.motion}/>
+        {this.props.door}
+      </Content>
     );
   }
 }
